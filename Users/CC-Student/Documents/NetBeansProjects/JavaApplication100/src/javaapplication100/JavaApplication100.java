1e9984d61eddfc493d6ada1f4c6b36ffee1b5533
oday <oday.jazraoui@yahoo.com>
2021-03-06 12:18:00 +0100

    Form für Slider

---
+++ Users/CC-Student/Documents/NetBeansProjects/JavaApplication100/src/javaapplication100/JavaApplication100.java
@@ -0,0 +1,11 @@
+package javaapplication100;
+
+public class JavaApplication100 {
+
+    public static void main(String[] args) {
+        Test fram = new Test();
+        fram.setVisible(true);
+        fram.setLocationRelativeTo(null);
+    }
+
+}
